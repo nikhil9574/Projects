22ecba3405b6b967e0e7394dd0e995f4d7bac620
nikhil prajapati <nikhilprajapati2106@gmail.com>
2025-05-22 23:43:57 +0530

    nothing changed

--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const  images =[
+    'src/assets/contact.png',
+    'src/assets/img1.jpeg'
+
+]
 
 export const Projects = () => {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const prevSlide = () => {
+        const newIndex = (currentIndex - 1 + images.length) % images.length;
+        setCurrentIndex(newIndex);
+    };
+
+    const nextSlide = () => {
+        const newIndex = (currentIndex + 1) % images.length;
+        setCurrentIndex(newIndex);
+    };
+
+
     return (
         <div>
             <div className={'shareTech text-center text-black italic text-5xl pt-7'}>
